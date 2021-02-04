b033011c47bbdde03c2c72134eba74c0bcc5b0a8
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2021-02-04 12:33:49 -0600

    Fix first character test

--- test_camel_case.py
+++ test_camel_case.py
@@ -6,6 +6,7 @@ class TestCamelCase(TestCase):
 	def test_first_character_cannot_be_number(self):
 		bad_string = '4this is a sentence'
 
-		result = camel_case.check_first_character
+		result = camel_case.check_first_character(bad_string)
 
 		self.assertTrue(result)
+
